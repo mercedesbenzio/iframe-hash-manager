f657025caff8d0570a41dd979691e7f7735eb103
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-08-16 12:29:42 +0200

    Enhance the build to support IE11
    The dist file, if included, was just crashing there. Also enhance the stack to support the latest WebPack and Babel standards

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
-var webpack = require('webpack')
-var path = require('path')
+const path = require('path')
 
-module.exports = function (options) {
-  options = options || {}
-  return {
-    entry: './src',
-    resolve: {
-      extensions: [ '.es6', '.js' ]
-    },
-    output: {
-      filename: 'iframeHashManager.js',
-      path: path.resolve(__dirname, 'dist'),
-      library: "iframeHashManager",
-      libraryTarget: "umd"
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loaders: ['babel-loader']
+module.exports = {
+  entry: './src/',
+  resolve: {
+    extensions: [ '.es6', '.js' ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'iframeHashManager.js',
+    library: 'iframeHashManager',
+    libraryTarget: "umd2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.es6$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            "presets": [
+              ["env", {
+                "targets": {
+                  "browsers": ["last 2 versions", "ie >= 11"]
+                }
+              }]
+            ],
+            "plugins": ["add-module-exports"]
+          }
         }
-      ]
-    }
+      }
+    ]
   }
 }
