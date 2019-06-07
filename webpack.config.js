0de0db707d79effd1677288cfbdc5ec3f61a9266
Nuno Costa <nuno.costa@mercedes-benz.io>
2019-06-07 09:04:25 +0100

    Upgrade all dev-dependencies (and respective configs) to latest versions, fixing vulnerabilities; logic.spec unit-test fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,24 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/',
   resolve: {
     extensions: [ '.es6', '.js' ]
   },
+  target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
     filename: 'iframeHashManager.js',
-    library: 'iframeHashManager',
-    libraryTarget: "umd2"
+    library: 'iframeHashManager'
   },
   module: {
     rules: [
       {
         test: /\.es6$/,
         exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            "presets": [
-              ["env", {
-                "targets": {
-                  "browsers": ["last 2 versions", "ie >= 11"]
-                }
-              }]
-            ],
-            "plugins": ["add-module-exports"]
-          }
-        }
+        loader: 'babel-loader'
       }
     ]
   }
